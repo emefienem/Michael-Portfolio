4014ebaf110d5500314424f07c402f85b108f4b1
emefienem <emefienemmichael@gmail.com>
2024-11-22 16:15:43 +0100

    Addition

--- src/pages/api/track-visit.ts
+++ src/pages/api/track-visit.ts
@@ -5,6 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "POST") {
     const { url, timestamp } = req.body;
 
