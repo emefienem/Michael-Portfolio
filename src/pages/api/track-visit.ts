94c0cc0673975cdbf652cba9fbaac3102492326a
emefienem <emefienemmichael@gmail.com>
2024-11-22 17:14:48 +0100

    Addition

--- src/pages/api/track-visit.ts
+++ src/pages/api/track-visit.ts
@@ -24,7 +24,7 @@ export default async function handler(
       console.log("Email sent: ", sendResult);
 
       res.status(200).json({ message: "Notification sent!" });
-    } catch (error: any) {
+    } catch (error: string) {
       console.error("Error sending email:", error);
       return res.status(500).json({ message: error.message });
     }
